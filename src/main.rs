eedc550ac291b77f941773d7d3f3e645f32f0cda
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2023-01-14 11:46:28 +0100

    Add simple listen on port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Server starting.");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    println!("Started listening on port 7878.");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
