c70b4ff19dea9b561430242ac9fc76d771edd667
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2023-01-14 12:00:21 +0100

    Add print request content

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    net::{TcpListener, TcpStream},
+    io::{prelude::*, BufReader},
+};
 
 fn main() {
     println!("Server starting.");
@@ -9,5 +12,17 @@ fn main() {
         let stream = stream.unwrap();
 
         println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
