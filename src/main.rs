da4002df3f245a799b8a34a0f1e6561560187e30
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2023-01-21 22:06:01 +0100

    Add check request URL
    Return hello only when '/' is requested

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,21 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    if http_request.len() > 0 {
+        // The first line contains request type
+        let request_line = http_request.first().unwrap();
 
-    stream.write_all(response.as_bytes()).unwrap();
+        if request_line == "GET / HTTP/1.1" {
+            let status_line = "HTTP/1.1 200 OK";
+            let contents = fs::read_to_string("hello.html").unwrap();
+            let length = contents.len();
+            let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+            stream.write_all(response.as_bytes()).unwrap();
+        } else {
+            // Some other request
+        }
+    } else {
+        println!("HTTP request is empty.");
+    }
 }
