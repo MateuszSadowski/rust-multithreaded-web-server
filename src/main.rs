37817f9d8539d8d0cdd4db1eed9caa72b611f2c2
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2023-01-21 22:24:07 +0100

    Refactor if-else statement

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,13 @@ fn handle_connection(mut stream: TcpStream) {
         // The first line contains request type
         let request_line = http_request.first().unwrap();
 
-        let mut status_line = "HTTP/1.1 404 NOT FOUND";
-        let mut contents = fs::read_to_string("404.html").unwrap();
-
-        if request_line == "GET / HTTP/1.1" {
-            status_line = "HTTP/1.1 200 OK";
-            contents = fs::read_to_string("hello.html").unwrap();
+        let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+            ("HTTP/1.1 200 OK", "hello.html")
         } else {
-            // some other request
-        }
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
 
+        let contents = fs::read_to_string(filename).unwrap();
         let length = contents.len();
         let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
