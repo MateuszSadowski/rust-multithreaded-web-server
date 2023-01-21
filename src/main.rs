caf9d43d1c3a7d6efc7d3a1e81bdf199669c0a29
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2023-01-21 21:45:56 +0100

    Add return html as response
    - Add hello.html
    - Extract port to a variable

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::{
+    fs,
     net::{TcpListener, TcpStream},
     io::{prelude::*, BufReader},
 };
 
 fn main() {
     println!("Server starting.");
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let port = 7878;
+    let listener = TcpListener::bind(format!("127.0.0.1:{port}")).unwrap();
 
-    println!("Started listening on port 7878.");
+    println!("Started listening on {port}.");
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
@@ -24,7 +26,10 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
