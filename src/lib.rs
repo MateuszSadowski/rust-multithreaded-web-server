29c94aea273a38ed82ab3c9408d858c776057dfc
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2023-01-22 14:30:08 +0100

    Add new and build implementaion for ThreadPool
    - Add ThreadPoolCreationError struct
    - Add doc comments for new and build

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,46 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
+    /// Create a new ThreadPool.
+    /// 
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
         ThreadPool
+    }
+
+    /// Create a new ThreadPool if size is larger than 0.
+    /// Returns a ThreadPoolCreationError otherwise.
+    /// 
+    /// The size is the number of threads in the pool.
+    pub fn build(size: usize) -> Result<ThreadPool, ThreadPoolCreationError> {
+        if size <= 0 {
+            return Err(ThreadPoolCreationError::new("size is smaller or equal 0".to_string()));
+        }
+
+        Ok(ThreadPool)
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+    }
+}
+
+pub struct ThreadPoolCreationError {
+    pub message: String,
+}
+
+impl ThreadPoolCreationError {
+    pub fn new(message: String) -> ThreadPoolCreationError {
+        Self {
+            message,
+        }
     }
 }
